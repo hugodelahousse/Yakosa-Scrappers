d287453d97d66e121cbda67ae986c7d32f06d4fd
Théo Issarni <issarnitheo@gmail.com>
2019-05-13 10:35:37 +0200

    scrapper: refacto from discution

--- src/scripts/anti-crise-scrapper.py
+++ src/scripts/anti-crise-scrapper.py
@@ -4,65 +4,63 @@ from datetime import timedelta
 from src.models.Promotion import Promotion
 from src.scripts.utils import simple_get, promotion_post
 from src.scripts.scrapper import Scrapper
-
-USER_ID = 1
-HISTORY_FILE = "../misc/history"
-API_URL = "https://yakosa.herokuapp.com/"
-URL = "http://anti-crise.fr/les-catalogues-avec-optimisations/"
-
-
-def get_history():
-    values = []
-    with open(HISTORY_FILE, "r") as file:
-        for line in file:
-            values.append(line[:len(line) - 1])
-    return values
-
-
-def extract_information(page, store_name, begin_date, end_date):
-    cases = page.find("div", {"id": "price-table"}) \
-        .find("div", {"id": "price-table-table"}) \
-        .find("table") \
-        .find_all("th")
-
-    position = {'PRODUIT': None, 'Q': None, 'PRIX': None, 'PROMO': None}
-
-    for i in range(len(cases)):
-        if 'PRODUIT' in cases[i].string:
-            position['PRODUIT'] = i
-        elif 'Q' in cases[i].string:
-            position['Q'] = i
-        elif 'PRIX' in cases[i].string:
-            position['PRIX'] = i
-        elif 'PROMO' in cases[i].string:
-            position['PROMO'] = i
-
-    promotion_array = page.find("div", {"id": "price-table"}) \
-        .find("div", {"id": "price-table-table"}) \
-        .find("table") \
-        .find_all("tr")
-    promotion_array = promotion_array[1:]
-
-    scraped_promotions = []
-
-    for promotion_data in promotion_array:
-        cell = promotion_data.find_all("td")
-        product_name = cell[position['PRODUIT']].string
-        product_price = cell[position['PRIX']].string.replace("€", "")
-        product_promo = cell[position['PROMO']].string.replace("€", "")
-        product_quantity = cell[position['Q']].string
-        promotion = Promotion(store_name, begin_date, end_date, product_name, product_price, product_promo, product_quantity)
-        scraped_promotions.append(promotion)
-
-    return scraped_promotions
+from src.scripts.config import API_URL, HISTORY_FILE
 
 
 class AntiCriseScrapper(Scrapper):
     time = timedelta(days=1)
+    url = "http://anti-crise.fr/les-catalogues-avec-optimisations/"
+
+    @staticmethod
+    def __get_history():
+        values = []
+        with open(HISTORY_FILE, "r") as file:
+            for line in file:
+                values.append(line[:len(line) - 1])
+        return values
+
+    @staticmethod
+    def __extract_information(page, store_name, begin_date, end_date):
+        cases = page.find("div", {"id": "price-table"}) \
+            .find("div", {"id": "price-table-table"}) \
+            .find("table") \
+            .find_all("th")
+
+        position = {'PRODUIT': None, 'Q': None, 'PRIX': None, 'PROMO': None}
+
+        for i in range(len(cases)):
+            if 'PRODUIT' in cases[i].string:
+                position['PRODUIT'] = i
+            elif 'Q' in cases[i].string:
+                position['Q'] = i
+            elif 'PRIX' in cases[i].string:
+                position['PRIX'] = i
+            elif 'PROMO' in cases[i].string:
+                position['PROMO'] = i
+
+        promotion_array = page.find("div", {"id": "price-table"}) \
+            .find("div", {"id": "price-table-table"}) \
+            .find("table") \
+            .find_all("tr")
+        promotion_array = promotion_array[1:]
+
+        scraped_promotions = []
+
+        for promotion_data in promotion_array:
+            cell = promotion_data.find_all("td")
+            product_name = cell[position['PRODUIT']].string
+            product_price = cell[position['PRIX']].string.replace("€", "")
+            product_promo = cell[position['PROMO']].string.replace("€", "")
+            product_quantity = cell[position['Q']].string
+            promotion = Promotion(store_name, begin_date, end_date, product_name, product_price, product_promo,
+                                  product_quantity)
+            scraped_promotions.append(promotion)
+
+        return scraped_promotions
 
     def fetch(self, url):
-        page = BeautifulSoup(simple_get(url), 'html.parser')
-        history = get_history()
+        page = BeautifulSoup(simple_get(self.url), 'html.parser')
+        history = self.__get_history()
 
         brand_names = []
         for brand_name in page.find_all("h2")[1:]:
@@ -93,9 +91,9 @@ class AntiCriseScrapper(Scrapper):
 
         for element in soup:
             page = BeautifulSoup(simple_get(element['url']), 'html.parser')
-            promotions = extract_information(page, element['brand'], element['begin_date'], element['end_date'])
+            promotions = self.__extract_information(page, element['brand'], element['begin_date'], element['end_date'])
             for scraped_promotion in promotions:
-                converted = scraped_promotion.convertToPromotion(API_URL, USER_ID)
+                converted = scraped_promotion.convertToPromotion(API_URL)
                 if converted is not None:
                     products.append(converted)
 
@@ -106,7 +104,7 @@ class AntiCriseScrapper(Scrapper):
         self.time = time
 
     def run(self):
-        soup = self.fetch(URL)
+        soup = self.fetch(None)
         promotions = self.transform(soup)
         for promotion in promotions:
             promotion_post(API_URL + 'promotions', promotion)
---
+++ src/scripts/config.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+HISTORY_FILE = Path(__file__).parent / "../misc/history"
+API_URL = "https://yakosa.herokuapp.com/"
