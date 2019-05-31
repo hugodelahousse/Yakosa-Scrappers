1b9e1d13c17db3b21924cadda81dd73c66ae83a4
Théo Issarni <issarnitheo@gmail.com>
2019-05-31 20:14:57 +0200

    scrapper: is fonctional and export stores in yml

--- src/scripts/config.py
+++ src/scripts/config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-HISTORY_FILE = Path(__file__).parent / "../misc/history"
-STORE_FILE = Path(__file__).parent / "../misc/stores"
-META_STORE_FILE = Path(__file__).parent / "../misc/meta_stores"
+BASE_PATH = Path(__file__).parent.parent
+HISTORY_FILE = BASE_PATH / "misc/history"
+STORE_FILE = BASE_PATH / "misc/stores"
+META_STORE_FILE_YML = BASE_PATH / "misc/meta_stores.yml"
 API_URL = "https://yakosa.herokuapp.com/"
